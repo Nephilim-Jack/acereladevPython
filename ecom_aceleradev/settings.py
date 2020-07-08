676ca2af2cb05053b34d2daa32fcaa9b0da47702
Matheus Mendes <ronaldo_rogerio@yahoo.com.br>
2020-07-08 13:18:26 -0300

    some changes

--- ecom_aceleradev/settings.py
+++ ecom_aceleradev/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'i1pi8sz9ddnpk8mblk1@^cj825e+19f_ucg%&eb@z+9s7&2mv9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
--- products/tests.py
+++ products/tests.py
@@ -1,14 +1,19 @@
 from django.test import TestCase
-from .models import Product
+from .models import Product, Category
 # Create your tests here.
 
 
 class ProductStrTestCase(TestCase):
     def test_str_should_return_name(self):
+        category = Category.objects.create(
+            name='Categoria Teste',
+            description='Descrição Teste'
+        )
         product = Product.objects.create(
             name='Teste Produto',
             description='Teste Description',
-            price=10.5
+            price=10.5,
+            category=category
         )
 
         self.assertEqual(str(product), 'Teste Produto')
